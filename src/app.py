0463b1cbf8d862f423c6fe58f515ec2256fad001
Markus Wahlman <markus.wahlman@helsinki.fi>
2024-11-18 14:11:17 +0200

    Add reset db path for tests

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import jsonify, render_template
 from config import app, test_env
+from db_helper import reset_db
 
 @app.route("/")
 def index():
     return render_template("index.html") 
+
+# Route for tests
+if test_env:
+    @app.route("/reset_db")
+    def reset_database():
+        reset_db()
+        return jsonify({ 'message': "db reset" })
