087aca4131570935bb6772ab26c389d5f5120db2
Kassudin <kasper.kinnarinen@live.fi>
2024-11-26 17:19:38 +0200

    Resolving merge

--- src/entities/citation.py
+++ src/entities/citation.py
@@ -1,3 +1,6 @@
+from bibtexparser.bibdatabase import BibDatabase
+from bibtexparser.bwriter import BibTexWriter
+
 class Citation:
     def __init__(self, id, type, author, title, year, booktitle=None, journal=None, volume=None, pages=None, publisher=None):
         self.id = id
@@ -10,6 +13,7 @@ class Citation:
         self.volume = volume
         self.pages = pages
         self.publisher = publisher
+        self.as_bibtex = self.to_bibtex()
 
     def __str__(self):
         details = [
@@ -30,3 +34,27 @@ class Citation:
         if self.publisher:
             details.append(f"Publisher: {self.publisher}")
         return "\n".join(details)
+    
+    def to_bibtex(self):
+        db = BibDatabase()
+        entry = {
+            "ID": str(self.id),
+            "ENTRYTYPE": self.type,
+            "author": self.author,
+            "title": self.title,
+            "year": str(self.year),
+        }
+        if self.booktitle:
+            entry["booktitle"] = self.booktitle
+        if self.journal:
+            entry["journal"] = self.journal
+        if self.volume:
+            entry["volume"] = self.volume
+        if self.pages:
+            entry["pages"] = self.pages
+        if self.publisher:
+            entry["publisher"] = self.publisher
+
+        db.entries = [entry]
+        writer = BibTexWriter()
+        return writer.write(db)
