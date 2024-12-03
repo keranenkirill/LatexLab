b2f9963b9fee643573d2fd4e17ecd857b13bb16e
Keränen Kirill <kirillke@cubbli24-gold.cs.helsinki.fi>
2024-12-03 22:26:54 +0200

    unittest refact

--- src/tests/u_test.py
+++ src/tests/u_test.py
@@ -1,5 +1,26 @@
 import unittest
+from unittest.mock import ANY, patch
 from util import UserInputError, validate_citation_form
+from repositories.citation_repository import del_citation, update_citation
+
+
+class TestDeleteCitation(unittest.TestCase):
+
+    @patch('repositories.citation_repository.db.session')
+    def test_delete_citation(self, mock_session):
+        mock_execute = mock_session.execute
+        mock_commit = mock_session.commit
+
+        citation_id = 1
+
+        del_citation(citation_id)
+
+        mock_execute.assert_called_once_with(
+            ANY,
+            {"id": citation_id}
+        )
+
+        mock_commit.assert_called_once()
 
 
 class TestCitationValidation(unittest.TestCase):
@@ -39,3 +60,33 @@ class TestCitationValidation(unittest.TestCase):
         self.valid_article_citation["year"] = "test"
         with self.assertRaises(UserInputError):
             validate_citation_form(self.valid_article_citation)
+
+
+class TestUpdateCitation(unittest.TestCase):
+
+    @patch('repositories.citation_repository.db.session')
+    def test_update_citation(self, mock_session):
+        mock_execute = mock_session.execute
+
+        citation_id = 1
+        citation = {
+            "type": "article",
+            "author": "auth",
+            "title": "titl",
+            "year": "2024",
+            "booktitle": "boog"
+        }
+
+        update_citation(citation_id, citation)
+
+        mock_execute.assert_called_once_with(
+            ANY,
+            {
+                "type": "article",
+                "author": "auth",
+                "title": "titl",
+                "year": "2024",
+                "booktitle": "boog",
+                "id": 1
+            }
+        )
