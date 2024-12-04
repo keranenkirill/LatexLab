5216f5af64a2f575be4df71af8e54773594ad191
Markus Wahlman <wahlman2002@gmail.com>
2024-12-04 13:36:49 +0200

    Change update citation test to partial match

--- src/tests/update_citation_test.py
+++ src/tests/update_citation_test.py
@@ -3,6 +3,15 @@ from unittest.mock import ANY
 from unittest.mock import patch
 from repositories.citation_repository import update_citation
 
+class PartialDictMatcher:
+    def __init__(self, expected):
+        self.expected = expected
+
+    def __eq__(self, other):
+        return all(other.get(key) == value for key, value in self.expected.items())
+
+    def __repr__(self):
+        return f"PartialDictMatcher({self.expected})"
 
 class TestUpdateCitation(unittest.TestCase):
 
@@ -24,12 +33,12 @@ class TestUpdateCitation(unittest.TestCase):
 
         mock_execute.assert_called_once_with(
             ANY,
-            {
+            PartialDictMatcher({
                 "type": "article",
                 "author": "auth",
                 "title": "titl",
                 "year": "2024",
                 "booktitle": "boog",
                 "id": 1
-            }
+            })
         )
